cc255f6146cc57e446b9950b13fd5d660c605bcb
Halo-Michael <yc992147156@me.com>
2023-12-01 00:01:51 +0800

    Update

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 xcodebuild clean build -quiet CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO PRODUCT_BUNDLE_IDENTIFIER="com.michael.networkcontroller" -sdk iphoneos -configuration Release
-ldid -Snetworkcontroller/networkcontroller.entitlements -Icom.michael.networkcontroller build/Release-iphoneos/Networkcontroller.app/Networkcontroller
+ldid -Snetworkcontroller/networkcontroller.entitlements -Icom.michael.networkcontroller build/Release-iphoneos/NetworkController.app/NetworkController
 cd build/Release-iphoneos
 ln -s ./ Payload
-zip -r9 Networkcontroller.ipa Payload/Networkcontroller.app
-mv Networkcontroller.ipa ../..
+zip -r9 NetworkController.ipa Payload/NetworkController.app
+mv NetworkController.ipa ../..
